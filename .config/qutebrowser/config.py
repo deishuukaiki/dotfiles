38abcf836f61e05b1e12c76487f5335e01025365
Deishuu Kaiki <maciek54134@tutanota.com>
2022-02-01 14:15:17 +0100

    qutebrowser fixes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,7 +4,6 @@ config.load_autoconfig(False)
 # config.set("colors.webpage.darkmode.enabled", True)
 
 # keepassxc extension
-# read https://github.com/ususdei/qute-keepassxc
 config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key 08C603F6', mode='insert')
 config.bind('pw', 'spawn --userscript qute-keepassxc --key 08C603F6', mode='normal')
 
@@ -21,10 +20,9 @@ config.set('content.cookies.accept', 'no-3rdparty')
 config.set('content.cookies.store', True)
 
 # user-agent
-config.set('content.headers.user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0')
-
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}')
 # don't allow notifications
-config.set('content.notifications.enabled', False)
+# config.set('content.notifications.enabled', False)
 
 # download dir
 c.downloads.location.directory = '~/downloads'
@@ -35,11 +33,11 @@ c.url.start_pages = 'https://deishuukaiki.github.io/homepage'
 c.url.searchengines = {'DEFAULT': 'https://searx.be/search?q={}'}
 
 # fonts and colors
-c.fonts.default_family = '"Monospace"'
-c.fonts.default_size = '10pt'
-c.fonts.prompts = 'default_size sans-serif'
+# c.fonts.default_family = '"Monospace"'
+# c.fonts.default_size = '10pt'
+# c.fonts.prompts = 'default_size sans-serif'
 config.source('nord-qutebrowser.py')
 
 # privacy
-config.set('content.canvas_reading', False)
+config.set('content.canvas_reading', True)
 config.set('content.webgl', False)
