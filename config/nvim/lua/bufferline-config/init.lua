71824d5c67be4435a77aaa5756f1451bdd80e0af
deishuukaiki <maciek54134@tutanota.com>
2022-04-08 11:22:03 +0200

    added kitty config

--- config/nvim/lua/bufferline-config/init.lua
+++ config/nvim/lua/bufferline-config/init.lua
@@ -2,7 +2,7 @@ require("bufferline").setup{
     options = {
         show_buffer_close_icons = false,
         show_close_icon = false,
-        separator_style = "padded_slant",
+        separator_style = "slant",
     },
 }
 vim.cmd[[
