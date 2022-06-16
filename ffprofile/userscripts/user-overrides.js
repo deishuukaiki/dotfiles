627c6c8b53e2a8c7e690b89aa0d186294201e212
deishuukaiki <maciek54134@tutanota.com>
2022-06-16 12:50:32 +0200

    added hackernews best button userscript

---
+++ ffprofile/userscripts/user-overrides.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hackernews best button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  show best button in hackernews
+// @author       You
+// @match        https://news.ycombinator.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ycombinator.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementsByClassName("pagetop")[0].innerHTML = document.getElementsByClassName("pagetop")[0].innerHTML + '\n|\n<a href="best">best</a>'
+})();
